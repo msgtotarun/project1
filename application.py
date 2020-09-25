153b3b861afb28a68c61c87af671102288a18364
tarun <msgtotarun@gmail.com>
2020-09-25 14:18:53 +0530

    added login page and few css changes

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask,render_template,request,session
+from flask import Flask, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-active='active'
+active = 'active'
+
 app = Flask(__name__)
+
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -19,18 +21,29 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+
 @app.route("/")
 def index():
-    return render_template("index.html",home=active)
+    return render_template("index.html", home=active)
+
 
 @app.route("/register")
 def register():
-     return render_template('register.html',register=active)
-@app.route("/registration",methods=["POST"])
+    return render_template('register.html', register=active)
+
+@app.route("/login")
+def login():
+    return render_template('login.html', login=active)
+
+
+@app.route("/registration", methods=["POST"])
 def registration():
-    name=request.form.get("Username")
-    email=request.form.get("email")
-    password=request.form.get("password")
-    print(name,password,email)
-    return name+" ,"+email
-   
+    name = request.form.get("Username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    print(name, password, email)
+    return name+" , "+email
+
+@app.route("/loginNow", methods=["POST"])
+def loginNow():
+    return 'hello'
