dead057fa9a21c03e8043c7ffbe6ef577229cf7e
tarun <msgtotarun@gmail.com>
2020-09-26 16:41:01 +0530

    added admin page and connected to db

--- application.py
+++ application.py
@@ -1,20 +1,24 @@
 import os
-
-from flask import Flask, render_template, request, session
+import time
+from flask import Flask, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
+from sqlalchemy.exc import SQLAlchemyError
+from flask.helpers import flash
 active = 'active'
-
 app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Session(app)
 
 # Set up database
@@ -31,19 +35,47 @@ def index():
 def register():
     return render_template('register.html', register=active)
 
+
 @app.route("/login")
 def login():
     return render_template('login.html', login=active)
 
 
+@app.route("/admin")
+def admin():
+    data = []
+    data = db.query(User).order_by(User.Time_registered)
+    return render_template('admin.html', data=data.all())
+
+
 @app.route("/registration", methods=["POST"])
 def registration():
-    name = request.form.get("Username")
-    email = request.form.get("email")
-    password = request.form.get("password")
-    print(name, password, email)
-    return name+" , "+email
+    # Get table information
+    # If table don't exist, Create.
+    if not engine.dialect.has_table(engine, "USERS"):
+        User.__table__.create(bind=engine, checkfirst=True)
 
-@app.route("/loginNow", methods=["POST"])
+    rowName = ['Name', 'Username', 'Email', 'Password']
+    row = []
+    for i in rowName:
+        row.append(request.form.get(i))
+    print(row)
+    user = User(Name=row[0], Username=row[1], Email=row[2],
+                Password=row[3], Time_registered=time.ctime(time.time()))
+
+    try:
+        db.add(user)
+        db.commit()
+        flash('account created sucessfully')
+        # return Name+" ,"+Email+","+Time_registered
+        return render_template("msg.html", msg='Hi '+row[0]+", Your account is sucessfully created")
+    except:
+        return render_template("msg.html", msg="please fill all details or error occured")
+    finally:
+        db.remove()
+        db.close()
+
+
+@ app.route("/loginNow", methods=["POST"])
 def loginNow():
     return 'hello'
--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 db=SQLAlchemy()
 
-class register(db.Model):
-    __tablename__="register"
+class User(db.Model):
+    __tablename__="User"
     id=db.Column(db.Integer, primary_key=True)
-    Name=db.Column(db.String(16), nullable=False)
+    Name=db.Column(db.String(30), nullable=False)
     Username=db.Column(db.String(16), nullable=False)
-    Email=db.Column(db.String(16), nullable=False)
+    Email=db.Column(db.String(60), nullable=False)
     Password=db.Column(db.String(16), nullable=False)
+    Time_registered=db.Column(db.DateTime, nullable=False )
