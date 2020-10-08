4270c0c6b4fd7cc58837834f11dfe7e48322963a
tarun <msgtotarun@gmail.com>
2020-10-08 12:30:40 +0530

    bug fixes

--- static/script.js
+++ static/script.js
@@ -1,119 +1,118 @@
-const form = document.getElementById('Rverify');
-const Lform = document.getElementById('Lverify');
-const name = document.getElementById('Name');
-const username = document.getElementById('Username');
-const email = document.getElementById('Email');
-const password = document.getElementById('Password');
-const password2 = document.getElementById('RPassword');
-const check = document.getElementById('exampleCheck1');
-form.addEventListener('submit', e => {
-    if (checkInputs())
-        e.preventDefault();
-
+const form = document.getElementById("Rverify");
+const Lform = document.getElementById("Lverify");
+const name = document.getElementById("Name");
+const username = document.getElementById("Username");
+const email = document.getElementById("Email");
+const password = document.getElementById("Password");
+const password2 = document.getElementById("RPassword");
+const check = document.getElementById("exampleCheck1");
+form.addEventListener("submit", (e) => {
+  if (checkInputs()) e.preventDefault();
 });
 
-Lform.addEventListener('submit', e => {
-    if (checkLogin())
-        e.preventDefault();
+Lform.addEventListener("submit", (e) => {
+  if (checkLogin()) e.preventDefault();
 });
 
 function checkLogin() {
-    const usernameValue = username.value.trim();
-    const passwordValue = password.value.trim();
-    var f = 1
-    if (usernameValue === '') {
-        setErrorFor(username, 'Username cannot be blank');
-        f = 0
-    } else {
-        setSuccessFor(Username);
-    }
-    if (passwordValue === '') {
-        setErrorFor(Password, 'Password cannot be blank');
-        f = 0
-    } else {
-        setSuccessFor(Password);
-    }
-    if (f = 0)
-        return false
-    else
-        return true
+  const usernameValue = username.value.trim();
+  const passwordValue = password.value.trim();
+  var f = 1;
+  if (usernameValue === "") {
+    setErrorFor(username, "Username cannot be blank");
+    f = 0;
+  } else {
+    setSuccessFor(Username);
+  }
+  if (passwordValue === "") {
+    setErrorFor(Password, "Password cannot be blank");
+    f = 0;
+  } else {
+    setSuccessFor(Password);
+  }
+  if ((f = 0)) return false;
+  else return true;
 }
 
 function checkInputs() {
-    // trim to remove the whitespaces
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-    var f = 1
-    let passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-    if (name.value === '') {
-        setErrorFor(name, 'name cannot be blank');
-        f = 0
-    } else {
-        setSuccessFor(name);
-    }
+  // trim to remove the whitespaces
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+  var f = 1;
 
-    if (usernameValue === '') {
-        setErrorFor(username, 'Username cannot be blank');
-        f = 0
-    } else if (isEmail(usernameValue)) {
-        setErrorFor(username, 'Username cannot be an Email');
-        f = 0
-    } else {
-        setSuccessFor(Username);
-    }
+  if (name.value === "") {
+    setErrorFor(name, "name cannot be blank");
+    f = 0;
+  } else {
+    setSuccessFor(name);
+  }
 
-    if (emailValue === '') {
-        setErrorFor(Email, 'Email cannot be blank');
-        f = 0
-    } else if (!isEmail(emailValue)) {
-        setErrorFor(Email, 'Not a valid email');
-        f = 0
-    } else {
-        setSuccessFor(Email);
-    }
-    if (!passwordValue.match(passw)) {
-        setErrorFor(password, 'password has to be at least 6 characters, with at least one uppercase letter, one lowercase letter, one number and a symbol');
-    } else
-        setSuccessFor(password)
+  if (usernameValue === "") {
+    setErrorFor(username, "Username cannot be blank");
+    f = 0;
+  } else if (isEmail(usernameValue)) {
+    setErrorFor(username, "Username cannot be an Email");
+    f = 0;
+  } else {
+    setSuccessFor(Username);
+  }
 
-    if (password2Value === '') {
-        setErrorFor(RPassword, 'Retype Password cannot be blank');
-        f = 0
-    } else if (passwordValue !== password2Value) {
-        setErrorFor(Rpassword, 'Passwords does not match');
-        f = 0
-    } else {
-        setSuccessFor(Rpassword);
-    }
+  if (emailValue === "") {
+    setErrorFor(Email, "Email cannot be blank");
+    f = 0;
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(Email, "Not a valid email");
+    f = 0;
+  } else {
+    setSuccessFor(Email);
+  }
+  console.log(checkPass(passwordValue));
+  if (!checkPass(passwordValue)) {
+    setErrorFor(
+      password,
+      "password has to be at least 6 characters, with at least one uppercase letter, one lowercase letter, one number and a symbol"
+    );
+  } else setSuccessFor(password);
 
+  if (password2Value === "") {
+    setErrorFor(RPassword, "Retype Password cannot be blank");
+    f = 0;
+  } else if (passwordValue !== password2Value) {
+    setErrorFor(Rpassword, "Passwords does not match");
+    f = 0;
+  } else {
+    setSuccessFor(Rpassword);
+  }
 
-
-    if (!check.checked) {
-        setErrorFor(check, 'agree terms and conditions');
-        f = 0
-    } else
-        setSuccessFor(check);
-    if (f = 0)
-        return false
-    else
-        return true
+  if (!check.checked) {
+    setErrorFor(check, "agree terms and conditions");
+    f = 0;
+  } else setSuccessFor(check);
+  if ((f = 0)) return false;
+  else return true;
 }
 
 function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    small.innerText = message;
-    small.style.color = 'red'
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+  small.style.color = "red";
 }
 
 function setSuccessFor(input) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    small.innerText = '';
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  small.innerText = "";
 }
-
+function checkPass(pass) {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&.])[A-Za-z\d$@$!%*?&.]{6,20}/.test(
+    pass
+  );
+}
 function isEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
